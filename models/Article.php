9a1f443daa3660ada045d2dd4e0355e09e6b33f1
Сергей Суховерхов <geekboox@yandex.ru>
2016-05-20 16:49:25 +0300

    add Article model

---
+++ models/Article.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $text
+ */
+class Article extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['text'], 'string'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'text' => 'Text',
+        ];
+    }
+}
