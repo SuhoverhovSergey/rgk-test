7425ceb3567f095902cd113be954ea544df19fa8
Сергей Суховерхов <geekboox@yandex.ru>
2016-05-20 12:18:33 +0300

    add local config for db

--- config/db.php
+++ config/db.php
@@ -1,9 +1,14 @@
 <?php
 
-return [
+$dbConfig = [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
 ];
+
+return yii\helpers\ArrayHelper::merge(
+    $dbConfig,
+    require(__DIR__ . '/db-local.php')
+);
