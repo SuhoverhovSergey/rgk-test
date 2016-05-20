ec100cbe1286b36ad82fda098bf611b33177b794
Сергей Суховерхов <geekboox@yandex.ru>
2016-05-20 12:44:15 +0300

    fix user table

--- migrations/m160520_080738_create_user.php
+++ migrations/m160520_080738_create_user.php
@@ -16,7 +16,11 @@ class m160520_080738_create_user extends Migration
             'id' => $this->primaryKey(),
             'username' => $this->string(255)->notNull()->unique(),
             'password' => $this->string(255)->notNull(),
+            'authKey' => $this->string(255)->notNull(),
+            'accessToken' => $this->string(255)->notNull(),
+            'created' => $this->timestamp()->notNull()->defaultExpression('CURRENT_TIMESTAMP'),
             'active' => $this->boolean()->notNull()->defaultValue(true),
+            'deleted' => $this->boolean()->notNull()->defaultValue(false),
         ]);
     }
 
