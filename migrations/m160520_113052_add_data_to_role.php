6a4b674b967dc5d0caf630314c8bc266270a9e23
Сергей Суховерхов <geekboox@yandex.ru>
2016-05-20 15:30:38 +0300

    add roles

---
+++ migrations/m160520_113052_add_data_to_role.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding data to table `role`.
+ */
+class m160520_113052_add_data_to_role extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $rows = [
+            [1, 'Администратор'],
+            [2, 'Пользователь'],
+        ];
+        $this->batchInsert('role', ['id', 'name'], $rows);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->delete('role');
+    }
+}
---
+++ models/Role.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "role".
+ *
+ * @property integer $id
+ * @property string $name
+ *
+ * @property User[] $users
+ */
+class Role extends ActiveRecord
+{
+    const ROLE_ADMIN = 1;
+    const ROLE_USER = 2;
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'role';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUsers()
+    {
+        return $this->hasMany(User::className(), ['role_id' => 'id']);
+    }
+}
